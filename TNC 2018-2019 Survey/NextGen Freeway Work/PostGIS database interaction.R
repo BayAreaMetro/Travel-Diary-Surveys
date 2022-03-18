4b8352975419ee2f444a869d59bb3d44446574e0
Shimon Israel <sisrael@bayareametro.gov>
2022-03-17 20:34:43 -0700

    Establish Postgres connection and verify PostGIS extension

--- TNC 2018-2019 Survey/NextGen Freeway Work/PostGIS database interaction.R
+++ TNC 2018-2019 Survey/NextGen Freeway Work/PostGIS database interaction.R
@@ -7,3 +7,12 @@ library(tidyverse)
 password_in <- readLines("M:/Data/HomeInterview/TNC Survey/SFCTA Map Matching/Password.txt")
 conn <- RPostgreSQL::dbConnect("PostgreSQL", host = "localhost",
                                dbname = "tncdata", user = "postgres", password = password_in)
+
+# Check that PostGIS extension is installed
+
+pgPostGIS(conn)
+
+# Disconnect from PostgreSQL session
+
+RPostgreSQL::dbDisconnect(conn)
+
