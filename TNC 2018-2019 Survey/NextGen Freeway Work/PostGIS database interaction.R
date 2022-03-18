ee8fff5e389927dcf917ddd2b1fded614b73b607
Shimon Israel <sisrael@bayareametro.gov>
2022-03-17 19:50:58 -0700

    Update credentials

--- TNC 2018-2019 Survey/NextGen Freeway Work/PostGIS database interaction.R
+++ TNC 2018-2019 Survey/NextGen Freeway Work/PostGIS database interaction.R
@@ -1,4 +1,9 @@
+library(RPostgreSQL)
+library(rpostgis)
+library(tidyverse)
 
-password <- readlines("M:/Data/HomeInterview/TNC Survey/SFCTA Map Matching/Password.txt")
+
+
+password_in <- readLines("M:/Data/HomeInterview/TNC Survey/SFCTA Map Matching/Password.txt")
 conn <- RPostgreSQL::dbConnect("PostgreSQL", host = "localhost",
-                               dbname = "tncdata", user = "postgres", password = "<PASSWORD>")
+                               dbname = "tncdata", user = "postgres", password = password_in)
