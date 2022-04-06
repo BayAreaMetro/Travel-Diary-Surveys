134440fada220e57a7fd6238398be061e12ffd42
Shimon Israel <sisrael@bayareametro.gov>
2022-04-05 23:01:55 -0700

    Develop flag values for hh, person, trip, facility combination

--- TNC 2018-2019 Survey/NextGen Freeway Work/Join OSM and TNC Survey Paths Files and Create Facility Flags.R
+++ TNC 2018-2019 Survey/NextGen Freeway Work/Join OSM and TNC Survey Paths Files and Create Facility Flags.R
@@ -1,5 +1,8 @@
 # Join OSM and TNC Survey Paths Files and Create Facility Flags.R
 # Compile all the freeway files and create flags for links on each facility
+# Get rid of scientific notation, bring in library
+
+options(scipen = 999)
 
 library(tidyverse)
 
@@ -90,7 +93,7 @@ Mar_Son_101_12To580 <- read.csv(file.path(segment_in,"Mar_Son_101_12To580.csv"))
 # Bind all segments together into a single file to merge with paths file
 # Receiving error messages when trying to bind them all in one step, so did it in increments
 
-segment1 <- bind_rows(
+all_segments <- bind_rows(
   Al_SF_80_PlazaTo101,
   SF_101_80ToSM,
   SF_280_StartToSM,
@@ -102,13 +105,9 @@ segment1 <- bind_rows(
   SC_280_SMTo101,
   Al_SC_680_101To580,
   Al_SC_880_101To238,
-  Al_880_238ToPlaza)
-
-segment2 <- rbind(
+  Al_880_238ToPlaza,
   Al_580_SanJoaquinTo238,
-  Al_580_238To80)
-
-segment3 <- bind_rows(
+  Al_580_238To80,
   Al_80_580ToPlaza,
   Al_CC_80_4To580,
   CC_Al_24_680To580,
@@ -116,11 +115,7 @@ segment3 <- bind_rows(
   CC_4_80To680,
   Sol_80_YoloToCarquinez,
   North_37_101To80,
-  Mar_Son_101_12To580)
-
-temp <- rbind(segment1,segment2)
-
-all_segments <- rbind(temp,segment3) %>% 
+  Mar_Son_101_12To580) %>% 
   select("GID", "CLASS_ID", "NAME", "SOURCE", "TARGET", "X1", "Y1", "X2", "Y2", 
            "ONE_WAY", "OSM_ID", "SOURCE_OSM", "TARGET_OSM", "facility_links")
 
@@ -147,12 +142,50 @@ rm(Al_SF_80_PlazaTo101,
    CC_4_80To680,
    Sol_80_YoloToCarquinez,
    North_37_101To80,
-   Mar_Son_101_12To580,
-   segment1,segment2,segment3)
+   Mar_Son_101_12To580)
 
 # Join segments to paths
+# Retain all paths with non-na freeway values
+# Summarize links by hh, person, trip ID, and facility
+# Pivot from long to wide to better understand trip and facility relationship
+# Reorder variables using select function, to match order used in project
+# Convert trip values into 0,1 by recoding values above 1 to 1
+# This step is to create a flag (0,1) for whether a given trip used a particular facility
 
 joined <- left_join(TNC_Survey_Paths,all_segments,by=c("edge"="GID")) %>% 
   filter(!(is.na(facility_links)))
 
+sum_links <- joined %>% 
+  group_by(hh_id,person_id,trip_id,facility_links) %>% 
+  summarize(total=n()) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = facility_links,values_from = total,values_fill = 0) %>% 
+  select(hh_id,person_id,trip_id,
+         Al_SF_80_PlazaTo101,
+         SF_101_80ToSM,
+         SF_280_StartToSM,
+         SM_101_SFToSC,
+         SM_280_SFToSC,
+         SC_101_SMTo680,
+         SC_101_680ToGilroy,
+         SC_237_101To880,
+         SC_280_SMTo101,
+         Al_SC_680_101To580,
+         Al_SC_880_101To238,
+         Al_880_238ToPlaza,
+         Al_580_SanJoaquinTo238,
+         Al_580_238To80,
+         Al_80_580ToPlaza,
+         Al_CC_80_4To580,
+         CC_Al_24_680To580,
+         CC_Al_680_4To580,
+         CC_4_80To680,
+         Sol_80_YoloToCarquinez,
+         North_37_101To80,
+         Mar_Son_101_12To580)
+
+
+final <- sum_links %>% 
+  mutate_at(vars(-hh_id, -person_id, -trip_id),  
+             ~if_else(.>=1,1,0))
 
