ed0ba0c210fbb9298519d07df21c6da46c452370
Shimon Israel <sisrael@bayareametro.gov>
2022-04-05 18:59:35 -0700

    Update Join OSM and TNC Survey Paths Files and Create Facility Flags.R

--- TNC 2018-2019 Survey/NextGen Freeway Work/Join OSM and TNC Survey Paths Files and Create Facility Flags.R
+++ TNC 2018-2019 Survey/NextGen Freeway Work/Join OSM and TNC Survey Paths Files and Create Facility Flags.R
@@ -3,12 +3,22 @@
 
 library(tidyverse)
 
-# Input OSM segments
+# Input segment directory
 
 dir1        <- "M:/Data/HomeInterview/TNC Survey/SFCTA Map Matching/NextGen Freeway Project"
 segment_in  <- file.path(dir1,"TNC_Survey_OSM_Network")
 
-# Individual segment files
+# Bring in TNC Survey paths file
+
+paths_in <- "M:/Data/HomeInterview/TNC Survey/SFCTA Map Matching/TNC_Survey_Paths.RData"
+paths <- load(paths_in)
+
+TNC_Survey_Paths <- TNC_Survey_Paths %>% 
+  select("m_lat", "m_lon", "mode_type", "collected_at", "hh_id", "person_id", "trip_id", 
+          "edge", "source", "target", "reversed", "c_lat", "c_lon", "loc", 
+          "meters")
+
+# Individual segment files developed from ArcMap identification of relevant OSM segments
 # Append facility name associated with links
 
 Al_SF_80_PlazaTo101 <- read.csv(file.path(segment_in,"Al_SF_80_PlazaTo101.csv")) %>% 
@@ -110,4 +120,39 @@ segment3 <- bind_rows(
 
 temp <- rbind(segment1,segment2)
 
-all_segments <- rbind(temp,segment3)
+all_segments <- rbind(temp,segment3) %>% 
+  select("GID", "CLASS_ID", "NAME", "SOURCE", "TARGET", "X1", "Y1", "X2", "Y2", 
+           "ONE_WAY", "OSM_ID", "SOURCE_OSM", "TARGET_OSM", "facility_links")
+
+# Remove individual data frames to clean up workspace
+
+rm(Al_SF_80_PlazaTo101,
+   SF_101_80ToSM,
+   SF_280_StartToSM,
+   SM_101_SFToSC,
+   SM_280_SFToSC,
+   SC_101_SMTo680,
+   SC_101_680ToGilroy,
+   SC_237_101To880,
+   SC_280_SMTo101,
+   Al_SC_680_101To580,
+   Al_SC_880_101To238,
+   Al_880_238ToPlaza,
+   Al_580_SanJoaquinTo238,
+   Al_580_238To80,
+   Al_80_580ToPlaza,
+   Al_CC_80_4To580,
+   CC_Al_24_680To580,
+   CC_Al_680_4To580,
+   CC_4_80To680,
+   Sol_80_YoloToCarquinez,
+   North_37_101To80,
+   Mar_Son_101_12To580,
+   segment1,segment2,segment3)
+
+# Join segments to paths
+
+joined <- left_join(TNC_Survey_Paths,all_segments,by=c("edge"="GID")) %>% 
+  filter(!(is.na(facility_links)))
+
+
