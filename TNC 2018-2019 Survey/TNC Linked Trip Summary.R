77d2507098efa226a7a792b911432fc44a7352f1
Shimon Israel <sisrael@bayareametro.gov>
2022-04-18 12:28:20 -0700

    Create trip file with income and race appended for analysis

--- TNC 2018-2019 Survey/TNC Linked Trip Summary.R
+++ TNC 2018-2019 Survey/TNC Linked Trip Summary.R
@@ -8,34 +8,93 @@ suppressMessages(library(tidyverse))
 
 # Set up working directory
 
-file_location <- "M:/Data/HomeInterview/TNC Survey/Task Order 8 Additional Data Refinement/Final Deliverables/"
-wd <- "C:/Users/sisrael/Documents/GitHub/Travel-Diary-Surveys/TNC 2018-2019 Survey/"  # work directory
-setwd(wd)
-output <- "H:/Presentations/Planning Section 2021/"
+temp <- "M:/Data/HomeInterview/TNC Survey/Data/Task 8 Data Refinement Version"
+file_location <- file.path(temp,"Final Updated Dataset as of 10-18-2021","RSG_HTS_Oct2021_bayarea")
+
+USERPROFILE          <- gsub("\\\\","/", Sys.getenv("USERPROFILE"))
+BOX_TM               <- file.path(USERPROFILE, "Box", "Modeling and Surveys")
+Output               <- file.path(BOX_TM,"Share Data","bespoke","2023 TIP Investment Analysis")
 
 # Bring in data
 
-trip_location 	<- paste0(file_location,"Final Updated Dataset as of 4-1-2021/RSG_HTS_February2021_bayarea/trip_linked.tsv")
+trip_location 	<- file.path(file_location,"trip_linked.tsv")
+hh_location     <- file.path(file_location,"hh.tsv")
+person_location <- file.path(file_location,"person.tsv")
 
 trip            <- read_tsv(trip_location,col_names=TRUE)
+hh              <- read_tsv(hh_location,col_names=TRUE)
+person          <- read_tsv(person_location,col_names=TRUE)
+
+# HH income joiner for trip file
+# Recode income
+# Recode county FIPS to the county strata used for weighting the data
+
+income_joiner <- hh %>% 
+  select(hh_id,income_detailed,home_county_fips) %>% 
+  mutate(
+    income_rc=case_when(
+      income_detailed==1               ~ "1_under 25",
+      income_detailed==2               ~ "1_under 25",
+      income_detailed==3               ~ "2_25-50",
+      income_detailed==4               ~ "2_25-50",
+      income_detailed==5               ~ "3_50-75",
+      income_detailed==6               ~ "4_75-100",
+      income_detailed==7               ~ "5_100-150",
+      income_detailed==8               ~ "6_150+",
+      income_detailed==9               ~ "6_150+",
+      income_detailed==10              ~ "6_150+",
+      TRUE                             ~ "7_missing"
+    ),
+    county_rc=case_when(
+      home_county_fips==1              ~ "Alameda",
+      home_county_fips==13             ~ "Contra Costa",
+      home_county_fips==41             ~ "Marin",
+      home_county_fips==55             ~ "Napa and Sonoma",
+      home_county_fips==75             ~ "San Francisco",
+      home_county_fips==81             ~ "San Mateo",
+      home_county_fips==85             ~ "Santa Clara",
+      home_county_fips==95             ~ "Solano",
+      home_county_fips==97             ~ "Napa and Sonoma"
+    )
+  )
+
+# Race joiner from the person file
+# Filter out active participants and recode race/ethnicity to survey categories
+
+race_joiner <- person %>% 
+  filter(is_active_participant==1) %>% 
+  select(hh_id,person_id,raceeth_new_imputed) %>% 
+  raceeth_rc = case_when(
+    raceeth_new_imputed== -1                    ~ "6_missing",
+    raceeth_new_imputed== 1                     ~ "5_Hispanic",
+    raceeth_new_imputed== 2                     ~ "2_Black",
+    raceeth_new_imputed== 3                     ~ "3_Asian/PI",
+    raceeth_new_imputed== 4                     ~ "1_White",
+    raceeth_new_imputed== 5                     ~ "4_Other",
+  )
+
+# Recode by trip type and join to race and income joiner
+# Filter missing mode records
 
 final <- trip %>% 
-  filter(!(mode_type %in% c(-9998,995))) %>% 
+  filter(!(mode_type_imputed %in% c(-9998,995))) %>% 
   mutate(
     mode_rc=case_when(
-      mode_type==1                    ~ "1_walk",
-      mode_type==2                    ~ "2_bike",
-      mode_type==3                    ~ "3_car",
-      mode_type==4                    ~ "4_other",
-      mode_type==5                    ~ "5_transit",
-      mode_type==6                    ~ "4_other",
-      mode_type==7                    ~ "4_other",
-      mode_type==8                    ~ "4_other",
-      mode_type==9                    ~ "4_other",
-      mode_type==10                    ~ "3_car",
-      mode_type==11                    ~ "2_bike",
-      mode_type==12                    ~ "4_other",
-      mode_type==13                    ~ "4_other"))
+      mode_type_imputed==1                    ~ "1_roadway", # walk
+      mode_type_imputed==2                    ~ "1_roadway", # bike
+      mode_type_imputed==3                    ~ "1_roadway", # car
+      mode_type_imputed==4                    ~ "2_roadway", # taxi
+      mode_type_imputed==5                    ~ "2_transit", # transit
+      mode_type_imputed==6                    ~ "2_transit", # schoolbus
+      mode_type_imputed==7                    ~ "1_roadway", # other
+      mode_type_imputed==8                    ~ "1_roadway", # shuttle/vanpool
+      mode_type_imputed==9                    ~ "1_roadway", # TNC
+      mode_type_imputed==10                   ~ "3_car",
+      mode_type_imputed==11                   ~ "2_bike",
+      mode_type_imputed==12                   ~ "4_other",
+      mode_type_imputed==13                   ~ "4_other")) %>% 
+  left_join(.,race_joiner,by=c("hh_id","person_id")) %>% 
+  left_join(.,income_joiner,by="hh_id")
 
 summary <- final %>% 
   group_by(mode_rc) %>% 
