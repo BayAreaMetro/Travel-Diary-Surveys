300719ee0f18dbabb7cada195c2e4a9610ebff16
Shimon Israel <sisrael@bayareametro.gov>
2022-07-27 10:25:55 -0700

    Group income, summarize daily and monthly trips

--- TNC 2018-2019 Survey/TNC Survey Monthly Trips by Income.R
+++ TNC 2018-2019 Survey/TNC Survey Monthly Trips by Income.R
@@ -24,7 +24,7 @@ trip       <- read_tsv(trip_location,col_names=TRUE)
 household  <- read_tsv(hh_location,col_names=TRUE) 
 person  <- read_tsv(person_location,col_names=TRUE) 
 
-# Merge household income file, recode income
+# Merge household income file, recode income in groups
 
 hh_income <- household %>%
   select(hh_id,income_detailed)%>% 
@@ -37,46 +37,32 @@ hh_income <- household %>%
     income_detailed==999            ~ "6_Prefer not to answer",
     TRUE                            ~ "7_Miscoded")) 
 
+# Join with income and remove missing data
+
 working <- left_join(trip,hh_income, by="hh_id") %>% 
   filter(hh_income!="6_Prefer not to answer")
 
-# Number of persons within each income category
-# Start with trip list and get a list of unique people
+# Summarize trips by income category and hh/person
+# Join person weights
+# Daily trips is weighted trips/person weight
+# Create daily and monthly trip variables
 
 trip_persons <- working %>% 
-  group_by(hh_id,person_id) %>% 
+  group_by(hh_id,person_id,hh_income) %>% 
   summarize(num_trips=sum(daywt_alladult_7day)) %>% 
-  select(-num_trips) %>% 
   ungroup()
 
 person_join <- person %>% 
   select(hh_id,person_id,wt_alladult_7day)
 
 trip_person_joined <- left_join(trip_persons,person_join,by=c("hh_id","person_id")) %>% 
-  left_join(.,hh_income,by="hh_id")
+  mutate(daily_trips=if_else(num_trips==0,0,num_trips/wt_alladult_7day))
 
-person_income_summary <- trip_person_joined %>% 
+final <- trip_person_joined %>% 
   group_by(hh_income) %>% 
-  summarize(total_persons=sum(wt_alladult_7day))
+  summarize(median_person_daily=weighted.median(daily_trips,wt_alladult_7day),
+            median_person_monthly=median_person_daily*365/12)
+
+View(final)
   
-# Total mean person trips per day by income
-# Multiply by 365 and divide by 12 to get monthly trips
 
-mean_trips <- working %>%
-  group_by(hh_income) %>% 
-  summarize(total_trips=sum(daywt_alladult_7day)) %>% 
-  left_join(.,person_income_summary,by="hh_income") %>% 
-  mutate(trips_per_person_daily=total_trips/total_persons) %>% 
-  mutate(mean_monthly_trips=trips_per_person_daily*365/12)
-
-
-median_trips <- working %>%
-  group_by(hh_income) %>% 
-  summarize(median_daily_trips=median(daywt_alladult_7day,na.rm = TRUE)) %>% 
-  filter(hh_income !="6_Prefer not to answer") %>% 
-  mutate(median_monthly_trips=median_daily_trips*365/12)
-
-
-# Write out TNC-only trip file
-
-write.csv(tnc_only, "TNC_Pilot_Trips.csv", row.names = FALSE, quote = T)
