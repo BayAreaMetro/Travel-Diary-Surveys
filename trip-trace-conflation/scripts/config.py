8fd91318b1ed38871db6549235503a781149579a
Joshua Croff <jcroff@bayareametro.gov>
2024-07-18 17:28:25 -0700

    Update config.py

--- trip-trace-conflation/scripts/config.py
+++ trip-trace-conflation/scripts/config.py
@@ -1,4 +1,4 @@
-import os
+import pathlib
 import getpass
 
 # Define user
@@ -9,19 +9,14 @@ location_tbl = "location.csv"
 trip_tbl = "trip.csv"
 
 # Define Box System Root Directory
-box_dir = os.path.join("/Users", user, "Library", "CloudStorage", "Box-Box")
+if user=="lzorn":
+    box_dir = pathlib.Path("E:\Box")
+else:
+    box_dir = pathlib.Path(f"/Users/{user}/Library/CloudStorage/Box-Box")
 
 # Define BAUS directory on Box for .csv output files
-file_dir = os.path.join(
-    box_dir,
-    "Modeling and Surveys",
-    "Surveys",
-    "Travel Diary Survey",
-    "Biennial Travel Diary Survey",
-    "Data",
-    "2023",
-    "Full Unweighted 2023 Dataset",
-)
+file_dir = box_dir / "Modeling and Surveys" / "Surveys" / "Travel Diary Survey" / \
+    "Biennial Travel Diary Survey" / "Data" / "2023" / "Full Unweighted 2023 Dataset"
 
 # Define input file paths
 location_path = os.path.join(file_dir, location_tbl)
@@ -30,7 +25,5 @@ region_boundary_path = os.path.join(box_dir, "DataViz Projects", "Spatial Analys
 local_network_path = os.path.join(box_dir, "DataViz Projects", "Spatial Analysis and Mapping", "TDS Conflation", "Data", "bay_area_network.json")
 
 # Define output file paths
-out_file_path = os.path.join(
-    box_dir, "DataViz Projects", "Spatial Analysis and Mapping", "TDS Conflation", "Data"
-)
-gpkg_path = os.path.join(out_file_path, "tds_conflation_results.gpkg")
+out_file_path = box_dir / "DataViz Projects" / "Spatial Analysis and Mapping" / "TDS Conflation" / "Data"
+gpkg_path = out_file_path / "tds_conflation_results.gpkg"
