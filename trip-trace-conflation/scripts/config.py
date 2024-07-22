f7958cc7f90f2c717c6a7692b29ee29f62ac0370
Lisa Zorn <lzorn@bayareametro.gov>
2024-07-21 17:49:48 -0700

    Use pathlib

--- trip-trace-conflation/scripts/config.py
+++ trip-trace-conflation/scripts/config.py
@@ -1,4 +1,3 @@
-import os
 import pathlib
 import getpass
 
@@ -11,7 +10,7 @@ trip_tbl = "trip.csv"
 
 # Define Box System Root Directory
 if user=="lzorn":
-    box_dir = pathlib.Path("E:\Box")
+    box_dir = pathlib.Path("E:\\Box")
 else:
     box_dir = pathlib.Path(f"/Users/{user}/Library/CloudStorage/Box-Box")
 
@@ -20,11 +19,13 @@ file_dir = box_dir / "Modeling and Surveys" / "Surveys" / "Travel Diary Survey" 
     "Biennial Travel Diary Survey" / "Data" / "2023" / "Full Unweighted 2023 Dataset"
 
 # Define input file paths
-location_path = os.path.join(file_dir, location_tbl)
-trip_path = os.path.join(file_dir, trip_tbl)
-region_boundary_path = os.path.join(box_dir, "DataViz Projects", "Spatial Analysis and Mapping", "TDS Conflation", "Data", "bay_area_regional_boundary.geojson")
-local_network_path = os.path.join(box_dir, "DataViz Projects", "Spatial Analysis and Mapping", "TDS Conflation", "Data", "bay_area_network.json")
+location_path = file_dir / location_tbl
+trip_path = file_dir / trip_tbl
+tds_data_path =  box_dir / "DataViz Projects" / "Spatial Analysis and Mapping" / \
+    "TDS Conflation" / "Data"
+region_boundary_path = tds_data_path / "bay_area_regional_boundary.geojson"
+local_network_path = tds_data_path / "bay_area_network.json"
 
 # Define output file paths
-out_file_path = box_dir / "DataViz Projects" / "Spatial Analysis and Mapping" / "TDS Conflation" / "Data"
+out_file_path = tds_data_path
 gpkg_path = out_file_path / "tds_conflation_results.gpkg"
