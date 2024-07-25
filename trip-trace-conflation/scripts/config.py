2442cf9e11d206e86a3e281ef7cf21ac8cd55485
Joshua Croff <jcroff@bayareametro.gov>
2024-07-25 10:30:06 -0700

    Update config.py

--- trip-trace-conflation/scripts/config.py
+++ trip-trace-conflation/scripts/config.py
@@ -1,5 +1,5 @@
-import pathlib
 import getpass
+import pathlib
 
 # Define user
 user = getpass.getuser()
@@ -9,22 +9,31 @@ location_tbl = "location.csv"
 trip_tbl = "trip.csv"
 
 # Define Box System Root Directory
-if user=="lzorn":
+if user == "lzorn":
     box_dir = pathlib.Path("E:\\Box")
-elif user=="mtcpb":
+elif user == "mtcpb":
     box_dir = pathlib.Path("\\\\tsclient\\E\\Box")
 else:
     box_dir = pathlib.Path(f"/Users/{user}/Library/CloudStorage/Box-Box")
 
 # Define BAUS directory on Box for .csv output files
-file_dir = box_dir / "Modeling and Surveys" / "Surveys" / "Travel Diary Survey" / \
-    "Biennial Travel Diary Survey" / "Data" / "2023" / "Full Unweighted 2023 Dataset"
+file_dir = (
+    box_dir
+    / "Modeling and Surveys"
+    / "Surveys"
+    / "Travel Diary Survey"
+    / "Biennial Travel Diary Survey"
+    / "Data"
+    / "2023"
+    / "Full Unweighted 2023 Dataset"
+)
 
 # Define input file paths
 location_path = file_dir / location_tbl
 trip_path = file_dir / trip_tbl
-tds_data_path =  box_dir / "DataViz Projects" / "Spatial Analysis and Mapping" / \
-    "TDS Conflation" / "Data"
+tds_data_path = (
+    box_dir / "DataViz Projects" / "Spatial Analysis and Mapping" / "TDS Conflation" / "Data"
+)
 region_boundary_path = tds_data_path / "bay_area_regional_boundary.geojson"
 local_network_path = tds_data_path / "bay_area_network.json"
 
