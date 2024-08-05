4fee016e333590fda389f126acc766b0fab7ed12
Lisa Zorn <lzorn@bayareametro.gov>
2024-08-05 16:39:51 -0700

    Updating paths to use convex hull of region
    And move to M drive

--- trip-trace-conflation/scripts/config.py
+++ trip-trace-conflation/scripts/config.py
@@ -8,35 +8,26 @@ user = getpass.getuser()
 location_tbl = "location.csv"
 trip_tbl = "trip.csv"
 
-# Define Box System Root Directory
-if user == "lzorn":
-    box_dir = pathlib.Path("E:\\Box")
-elif user == "mtcpb":
-    box_dir = pathlib.Path("\\\\tsclient\\E\\Box")
-else:
-    box_dir = pathlib.Path(f"/Users/{user}/Library/CloudStorage/Box-Box")
-
-# Define BAUS directory on Box for .csv output files
-file_dir = (
-    box_dir
-    / "Modeling and Surveys"
-    / "Surveys"
-    / "Travel Diary Survey"
-    / "Biennial Travel Diary Survey"
-    / "Data"
-    / "2023"
-    / "Full Unweighted 2023 Dataset"
-)
+# M directory for survey work
+M_survey_data_dir = pathlib.Path("M:\\Data\\HomeInterview\\Bay Area Travel Study 2023\\Data")
 
 # Define input file paths
-location_path = file_dir / location_tbl
-trip_path = file_dir / trip_tbl
-tds_data_path = (
-    box_dir / "DataViz Projects" / "Spatial Analysis and Mapping" / "TDS Conflation" / "Data"
+location_path = M_survey_data_dir / "Full Unweighted 2023 Dataset" / location_tbl
+trip_path = M_survey_data_dir / "Full Unweighted 2023 Dataset" / trip_tbl
+
+region_boundary_path = (
+    M_survey_data_dir
+    / "Survey Conflation"
+    / "OSM_regional_network_convex_hull"
+    / "bay_area_regional_boundary_convex_hull.geojson"
 )
-region_boundary_path = tds_data_path / "bay_area_regional_boundary.geojson"
-local_network_path = tds_data_path / "bay_area_network.json"
+local_network_path = (
+    M_survey_data_dir
+    / "Survey Conflation"
+    / "OSM_regional_network_convex_hull"
+    / "bay_area_network.json"
+)
 
 # Define output file paths
-out_file_path = tds_data_path
+out_file_path = M_survey_data_dir / "Full Unweighted 2023 Dataset" / "OSM_match_v2"
 gpkg_path = out_file_path / "tds_conflation_results.gpkg"
