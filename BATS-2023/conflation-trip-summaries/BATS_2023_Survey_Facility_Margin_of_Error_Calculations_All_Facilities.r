e9f54007f0c6a031acd1601f7a39ec659560664f
Shimon Israel <sisrael@bayareametro.gov>
2024-08-26 14:40:29 -0700

    Update documentation

--- BATS-2023/conflation-trip-summaries/BATS_2023_Survey_Facility_Margin_of_Error_Calculations_All_Facilities.r
+++ BATS-2023/conflation-trip-summaries/BATS_2023_Survey_Facility_Margin_of_Error_Calculations_All_Facilities.r
@@ -1,5 +1,6 @@
 # BATS_2023_Survey_Facility_Margin_of_Error_Calculations_All_Facilities.r
-# Summarize BATS 2023 survey data for key variables and calculate margins of error
+# Summarize BATS 2023 survey data shares for key variables and calculate margins of error
+# Append PUMS 2022 data for Bay Area-wide comparisons
 # Remove all PII to develop a version for Tableau Public sharing
 
 # Set options to get rid of scientific notation
@@ -12,7 +13,7 @@ suppressMessages(library(tidyverse))
 library(sf)
 library(spatstat)
 
-# Set output directory
+# Set file directories for input and output
 
 USERPROFILE    <- gsub("////","/", Sys.getenv("USERPROFILE"))
 BOX_dir1       <- file.path(USERPROFILE, "Box", "Modeling and Surveys","Surveys","Travel Diary Survey")
@@ -23,10 +24,11 @@ output         <- file.path(Box_dir2,"Summaries")
 
 # Bring in BATS 2023 survey files
 # Select only the variables needed for this script
+# Get actual income values from income_detailed variable codes
 
 person         <- read.csv(file=file.path(data_loc,"person.csv")) %>% 
-  select(hh_id,person_id,relationship,grep("race|ethnicity|weight",names(.)))
-trip           <- read.csv(file=file.path(data_loc,"trip.csv")) %>%     #Drop hh_id for easier joining later on trip_id
+  select(hh_id,person_id,relationship,grep("race|ethnicity|weight",names(.)))    
+trip           <- read.csv(file=file.path(data_loc,"trip.csv")) %>%     #Drop hh_id for easier joining later with person file
   select(-hh_id)
 household      <- read.csv(file=file.path(data_loc,"hh.csv"))%>% 
   select(hh_id,num_people,income_detailed,income_imputed_rmove_only,grep("weight",names(.))) %>% 
@@ -46,14 +48,14 @@ household      <- read.csv(file=file.path(data_loc,"hh.csv"))%>%
   
 
 # Commenting out the below for possible future use in plotting freeways
-# Bring in freeway coordinates for placing pie charts on map
-# File also has long name equivalencies for facilities and Bay Area geographical designations
+    # Bring in freeway coordinates for placing pie charts on map
+    # File also has long name equivalencies for facilities and Bay Area geographical designations
 
-# freeway_coords_in <- file.path(Output,"Freeway Facility Coordinates.csv")
-# freeway_coords <- read.csv(freeway_coords_in)
+    # freeway_coords_in <- file.path(Output,"Freeway Facility Coordinates.csv")
+    # freeway_coords <- read.csv(freeway_coords_in)
 
-# Bring in facility flag file (file that indicates whether a given trip traverses a given freeway/bridge)
-# Create vector of facilities for analysis
+# Bring in facility flag file (file that indicates whether a trip traverses a given freeway/bridge)
+# Create vector of facilities (full_facilities_list) for passing into a later function
 
 facility_flag <- read.csv(file = file.path(conflation_loc,"BATS 2023 Facility Use Booleans.csv"))
 
@@ -78,11 +80,11 @@ full_facilities_list <- c("bay_bridge",
                           "i580_hayward_to_baybridge",
                           "i80_13_to_580",
                           "i80_580_to_Carquinez",
-                          "i80_680_to_12")
+                          "i80_680_to_12",
+                          "all_script_freeways")
                           
 
-# Function for imputing discrete value from PUMS distribution
-# Start by bringing in 2022 PUMS data
+# Bring in 2022 PUMS data, household and person files for various tasks
 
 HH_RDATA = "M:/Data/Census/PUMS/PUMS 2022/hbayarea22.Rdata"
 load (HH_RDATA)
@@ -90,21 +92,21 @@ load (HH_RDATA)
 PERSON_RDATA = "M:/Data/Census/PUMS/PUMS 2022/pbayarea22.Rdata"
 load (PERSON_RDATA)
 
-# Adjust income to inflation-correct values for 2019
+# Use PUMS 2022 adjustment variable to inflation-correct values for 2022 (data collected over 12 months, so provides a constant dollar value)
 # Remove group quarters and vacant housing
 
 bay_income <- hbayarea22  %>% 
-  mutate(adjustment = ADJINC/1000000,
+  mutate(adjustment = ADJINC/1000000,                        # Adjustment variable is 7 digits
          income=HINCP*adjustment)  %>%  
-  filter(!is.na(income)) %>% 
-  select(PUMA,SERIALNO,income,HINCP,ADJINC,adjustment,WGTP)
+  filter(!is.na(income)) %>%                                 # Remove records with no income (vacant houses, group quarters)
+  select(PUMA,SERIALNO,income,HINCP,ADJINC,adjustment,WGTP)  # Select relevant variables
 
-# Calculate area median income
+# Calculate Bay Area weighted median income for tabulating share of AMI categories
 
 bay_median <- weighted.median(x=bay_income$income,w=bay_income$WGTP)
 
 # Calculate share of PUMS households by percentage AMI
-# Calculate share of Bay Area persons below 200 percent poverty, exclude people with NA poverty calculation
+# Calculate share of Bay Area persons below 200 percent poverty, exclude people with NA poverty calculation (foster children, group quarters)
 
 bay_income_med <- bay_income  %>%  
 mutate(
@@ -129,12 +131,30 @@ bay_poverty <- pbayarea22 %>%
   mutate(PUMS_poverty_share=total/sum(total)) %>% 
   ungroup()
 
+# Get Bay Area race totals from PUMS person file for comparing to freeway facilities/bridges
 
-# Create income function that samples from appropriate PUMS bins to get a discrete income value from categorical data
-# Start with more detailed income (which has missing data) then use imputed records to catch missing records
-# The "TRUE" value below is for records with neither an income_detailed nor an income_imputed_rmove_only record
-# This chooses a record from the full PUMS dataset to match. In the existing TDS dataset, no such records exist, but
-# the line is inlcuded so the function runs in every case
+race22 <- pbayarea22 %>% 
+  mutate(race_recoded=case_when(
+    HISP>1               ~"Hispanic",
+    HISP==1 & RAC1P==1   ~"White",
+    HISP==1 & RAC1P==2   ~"Black",
+    HISP==1 & RAC1P==3   ~"Other",
+    HISP==1 & RAC1P==4   ~"Other",
+    HISP==1 & RAC1P==5   ~"Other",
+    HISP==1 & RAC1P==6   ~"Asian/Pacific Islander",
+    HISP==1 & RAC1P==7   ~"Asian/Pacific Islander",
+    HISP==1 & RAC1P>=8   ~"Other",
+    TRUE                 ~"Uncoded")) %>% 
+  group_by(race_recoded) %>% 
+  summarize(count=n(),total=sum(PWGTP)) %>% 
+  mutate(PUMS_race_share=total/sum(total))
+
+# Function for imputing discrete household income value for travel diary categories - using PUMS weight distribution
+# The discrete income value is guessed (imputed) in the tightest income category for which there is a categorical data response
+# Start with more detailed income (which has missing data) then use imputed records to addressing missing records
+# The "TRUE" value in the function below is for records with neither an income_detailed nor an income_imputed_rmove_only record
+# In such a condition, a record is chosen at random from the full PUMS dataset to match. 
+# In the existing TDS dataset, no such records exist, but the line is included so the function runs in every case (other datasets, etc.)
 
 set.seed(1)
 discrete_income_f <- function(income_detailed,income_imputed_rmove_only){
@@ -162,13 +182,14 @@ discrete_income_f <- function(income_detailed,income_imputed_rmove_only){
   return(value)
 }
 
-
-# 
+# Returning to travel diary datasets
+# Append a variable for related persons on the BATS 2023 person file (differs from HH size in some instances - roommates, etc.)
+# Join related persons count to person file for poverty calculation
 # Recode trip file using race/ethnicity from person file
 # Join with household file to append detailed_income and imputed income variables
-# Run discrete income function defined above to get a discrete income variable for each categorical income
-# Get share of regional median income and group those values into a new ami variable
-# Select significant variables of interest
+# Run discrete income function defined above to get a value for each categorical income
+# Get share of 2x poverty, regional AMI and group those values into new variables
+# Poverty thresholds come from Katey Hinkle 8/22/24 email to Shimon/Flavia
 
 related_persons <- person %>%
   filter(relationship != "6") %>%          # Remove unrelated persons
@@ -208,28 +229,7 @@ person_joiner <- person  %>%
       TRUE                                                             ~ "Miscoded"
     ))  
 
-# Get race from PUMS for comparing to freeway facilities
-
-PERSON_RDATA = "M:/Data/Census/PUMS/PUMS 2022/pbayarea22.Rdata"
-load (PERSON_RDATA)
-
-race22 <- pbayarea22 %>% 
-mutate(race_recoded=case_when(
-  HISP>1               ~"Hispanic",
-  HISP==1 & RAC1P==1   ~"White",
-  HISP==1 & RAC1P==2   ~"Black",
-  HISP==1 & RAC1P==3   ~"Other",
-  HISP==1 & RAC1P==4   ~"Other",
-  HISP==1 & RAC1P==5   ~"Other",
-  HISP==1 & RAC1P==6   ~"Asian/Pacific Islander",
-  HISP==1 & RAC1P==7   ~"Asian/Pacific Islander",
-  HISP==1 & RAC1P>=8   ~"Other",
-  TRUE                 ~"Uncoded")) %>% 
-  group_by(race_recoded) %>% 
-  summarize(count=n(),total=sum(PWGTP)) %>% 
-  mutate(PUMS_race_share=total/sum(total))
-
-# Recoded trip purpose on linked trip file
+# Recoding trip purpose from trip file
 
 recoded_trip <- trip %>% 
   mutate(
@@ -246,22 +246,22 @@ recoded_trip <- trip %>%
     )
   )
 
-# Join trips file with facility flag file 
-# Create flag for all freeways, used later to sum trip characteristics among full freeway network
+# Join trip file with facility flag file 
+# Create flag for all freeways included in this script, used later to sum trip characteristics among full study network (though not inclusive of all Bay Area freeways)
 # Remove trips that don't have rmove weights
 
 working <- left_join(facility_flag,recoded_trip,by="trip_id") %>% 
   left_join(.,person_joiner,by=c("person_id")) %>% 
-  mutate(All_Freeways=1) %>% 
-  relocate(All_Freeways,.after = "i80_680_to_12") %>% 
+  mutate(all_script_freeways=1) %>% 
+  relocate(all_script_freeways,.after = "i80_680_to_12") %>% 
   filter(trip_weight_rmove_only>0)
 
 # Function to analyze data and calculate standard errors
-# Filter for facility value==1 (i.e., traverses that facility) 
+# Filter for facility value==1 (i.e., traverses that specific freeway/bridge) 
 # Formula for SE of a weighted sample: 
 # https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Standard_error_of_a_proportion_estimation_when_using_weighted_data
 # Takes the form: sqrt(p(1-p)summation((weights standardized to 1)^2))
-# Do separate summaries for race, trip purpose, and income, then concatenate and do additional calculations
+# Do separate summaries for race, trip purpose, poverty, and AMI, then append records to database
 
 # Define tod=time of day using departure hour ("all_day","peak","am_peak","pm_peak","off_peak)
 # "all_day" is by definition all day and therefore not a subset/filtering of data
@@ -271,13 +271,11 @@ am_peak <- c(6:9)
 pm_peak <- c(15:18)
 off_peak <- c(0:5,10:14,19:23)
 
-# Now create function
-# df_tod=data frame used for that time of day
-# Facility is the roadway analyzed
-# To later calculate standard error of a weighted sample, as referenced above, 
+# Now create function for iterating by facility analyzed and time of day
+# The variagle squared_standard_weights is saved to later calculate standard error of a weighted sample, as referenced above. 
 # Weights for the subsetted facility need to be standardized to sum to 1 and then squared - done below
 
-calculations <- function(df,facility,tod){
+calculations <- function(df,facility,tod){       # Beginning of function
 temp_output <- data.frame()
 
 stopifnot(tod %in% c("all_day","peak","am_peak","pm_peak","off_peak"))
@@ -329,6 +327,7 @@ if (tod=="off_peak"){
     rename(metric=purpose_recoded) %>% 
     ungroup()
   
+# Commenting out below paragraph that summarizes output data by income categories (using the poverty thresholds in lieu for this script)
 #  income <- temp_df %>% 
 #    group_by(income_recoded) %>% 
 #    summarize(count=n(),total=sum(trip_weight_rmove_only),share_value=sum(trip_weight_rmove_only)/total_trips) %>% 
@@ -362,7 +361,7 @@ if (tod=="off_peak"){
     mutate(roadway=facility,
            standard_error=sqrt((share_value*(1-share_value)*error_summation)),
            ci_95=1.96*standard_error,
-           lower_bound=if_else(share_value-ci_95>=0,share_value-ci_95,0),
+           lower_bound=if_else(share_value-ci_95>=0,share_value-ci_95,0),     # Lower bound bottom codes at 0
            upper_bound=share_value+ci_95,
            range=upper_bound-lower_bound,
            cv=(standard_error/share_value)*100,  
@@ -378,7 +377,7 @@ if (tod=="off_peak"){
     relocate(time_period,.after = category)
   
   return(temp_output)
-}
+}                                               # End of function
 
 # Iterate function over all 22 facilities, by each time period, then bind all together
 
@@ -404,23 +403,24 @@ full_off_peak <- purrr::map_dfr(full_facilities_list, ~{calculations(df=working,
 
 temp <- bind_rows(full_all_day,full_peak,full_am_peak,full_pm_peak,full_off_peak)
 
-# Join PUMS household share of AMI to compare population shares vs. people driving on freeways
-# Rename "count" and "total" variables in final dataset to unweighted and weighted (more intuitive)
+# Join PUMS household/person shares of AMI, race, and poverty to compare population shares vs. people driving on freeways/bridges
+# Rename "count" and "total" variables in final dataset to "unweighted" and "weighted" (more intuitive)
+# Populate missing fields with NA so all variables will bind neatly with trip summary
 
 ami_joiner <- bay_income_med %>% 
-  mutate(roadway="Bay_Area_Population", category="ami_income",time_period="Bay Area Population",
+  mutate(roadway="Bay_Area_Population_2022", category="ami_income",time_period="Bay Area Population",
          standard_error=NA_real_,ci_95=NA_real_,lower_bound=NA_real_,upper_bound=NA_real_,
          range=NA_real_,cv=NA_real_,est_reliability=NA_character_) %>% 
   rename(metric=ami_recoded, share_value=PUMS_household_share) 
 
 race_joiner <- race22 %>% 
-  mutate(roadway="Bay_Area_Population", category="ethnicity",time_period="Bay Area Population",
+  mutate(roadway="Bay_Area_Population_2022", category="ethnicity",time_period="Bay Area Population",
          standard_error=NA_real_,ci_95=NA_real_,lower_bound=NA_real_,upper_bound=NA_real_,
          range=NA_real_,cv=NA_real_,est_reliability=NA_character_) %>% 
   rename(metric=race_recoded, share_value=PUMS_race_share) 
 
 poverty_joiner <- bay_poverty %>% 
-  mutate(roadway="Bay_Area_Population", category="poverty_status",time_period="Bay Area Population",
+  mutate(roadway="Bay_Area_Population_2022", category="poverty_status",time_period="Bay Area Population",
          standard_error=NA_real_,ci_95=NA_real_,lower_bound=NA_real_,upper_bound=NA_real_,
          range=NA_real_,cv=NA_real_,est_reliability=NA_character_) %>% 
   rename(metric=poverty_status, share_value=PUMS_poverty_share) 
