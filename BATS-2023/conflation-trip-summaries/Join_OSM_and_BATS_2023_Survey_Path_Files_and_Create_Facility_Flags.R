ef12bb359a5224d201fab5c914e352df529479cf
Shimon Israel <sisrael@bayareametro.gov>
2024-09-29 16:54:45 -0700

    Added express lanes recoded segments for 680 and 80

--- BATS-2023/conflation-trip-summaries/Join_OSM_and_BATS_2023_Survey_Path_Files_and_Create_Facility_Flags.R
+++ BATS-2023/conflation-trip-summaries/Join_OSM_and_BATS_2023_Survey_Path_Files_and_Create_Facility_Flags.R
@@ -41,23 +41,22 @@ hh                <- read.csv(hh_in) %>%
   select(hh_id,participation_group,hh_weight_rmove_only)
 person            <- read.csv(person_in)
 trip              <- read.csv(trip_in) %>%
-  select(trip_id,managed_lane_use,driver,num_travelers,num_hh_travelers,mode_type,mode_1,mode_2,trip_weight_rmove_only)
+  select(trip_id,managed_lane_use,driver,num_travelers,num_hh_travelers,mode_type,mode_1,mode_2,trip_weight_rmove_only) %>% 
+  mutate(express_lane_use=if_else(
+    mode_type==8 & managed_lane_use==1 &                                    # Must be mode_type 8 and managed_lane_use==1 and...
+      (driver==1 | num_travelers==1 | num_travelers==num_hh_travelers |     # Must either be driver==1, single occupant, or all occupants are hh members
+         (mode_1 %in% c(1,6,7,8) & mode_2 %in% c(1,6,7,8,995))),1,0))       # Or, vehicle must be a hh vehicle, walk leg
 
 
 # Join segments to paths
 # Create facilities with directionality
 # Retain all paths with non-na freeway values
-# Create express_lanes variable that indicates a household car is using an EL
 # Summarize links by trip ID and facility
 # Pivot from long to wide to better understand trip and facility relationship
 
 joined <- left_join(con_attr_df,facility_segments_direction,by="osmid") %>% 
   left_join(.,trip %>% select(trip_id,driver,managed_lane_use,num_travelers,num_hh_travelers,mode_type,mode_1,mode_2),by="trip_id") %>% 
   filter(!(is.na(Facility))) %>% 
-  mutate(express_lanes=if_else(
-    mode_type==8 & managed_lane_use==1 &                                    # Must be mode_type 8 and managed_lane_use==1 and...
-      (driver==1 | num_travelers==1 | num_travelers==num_hh_travelers |     # Must either be driver==1, single occupant, or all occupants are hh members
-         (mode_1 %in% c(1,6,7,8) & mode_2 %in% c(1,6,7,8,995))),1,0)) %>%   # Or, vehicle must be a hh vehicle, walk leg
   mutate(Facility_Final=case_when(
     Facility=="ant_bridge" & Direction=="northbound"      ~ "ant_bridge_toll",
     Facility=="ant_bridge" & Direction=="southbound"      ~ "ant_bridge_notoll",
@@ -83,9 +82,6 @@ joined <- left_join(con_attr_df,facility_segments_direction,by="osmid") %>%
     Facility=="gg_bridge" & Direction=="northbound"       ~ "gg_bridge_notoll",
     Facility=="gg_bridge" & Direction=="southbound"       ~ "gg_bridge_toll",
     
-    Facility=="i880_baybridge_to_237" & express_lanes==1  ~ "i880_baybridge_to_237_exp",
-    Facility=="i880_baybridge_to_237" & express_lanes==0  ~ "i880_baybridge_to_237_gp",
-    
     TRUE                                                  ~ .$Facility
   ))
 
@@ -99,11 +95,13 @@ sum_links <- joined %>%
 # Convert trip values into 0,1 by recoding values above 1 to 1
 # This step is to create a flag (0,1) for whether a given trip used a particular facility
 # sr37_80_to_101=1 if any component segments of 37 are utilized
-# i680_80_to_580=1 if i680_80_to_580_portion=1 (which is full extent minus bm_bridge) and/or bm_bridge=1
+# i680_80_to_580=1 if i680_80_to_bm_bridge==1, bm_bridge==1,and/or i680_bm_bridge_to_580==1
 # bata_bridges=1 if any of the seven BATA bridges=1 (not including gg_bridge)
 # Create facility values for toll,notoll, and combined directions (including toll or notoll)
+# Create express_lanes variable that indicates a household car is using an EL
 # The below list includes records with a zero rmove_only weight as they may be useful for other applications (maybe?);
 # Any analysis applying weights will zero out records accordingly
+# Use select to order facilities
 
 final <- sum_links %>% 
   mutate_at(vars(-trip_id),~if_else(.>=1,1,as.double(.))) %>% 
@@ -116,55 +114,60 @@ final <- sum_links %>%
          bm_bridge=if_else((bm_bridge_toll==1 | bm_bridge_notoll==1),1,0),
          ant_bridge=if_else((ant_bridge_toll==1 | ant_bridge_notoll==1),1,0),
          gg_bridge=if_else((gg_bridge_toll==1 | gg_bridge_notoll==1),1,0),
-         i680_80_to_580=if_else((i680_80_to_580_portion==1 | bm_bridge==1),1,0),
+         i680_80_to_580=if_else((i680_80_to_bm_bridge==1 | bm_bridge==1 | i680_bm_bridge_to_580==1),1,0),
          bata_bridges_toll=if_else(if_any(c(bay_bridge_toll,sm_bridge_toll,dum_bridge_toll,rsr_bridge_toll,
                                        carq_bridge_toll,bm_bridge_toll,ant_bridge_toll), ~ .x==1),1,0),
          bata_bridges_notoll=if_else(if_any(c(bay_bridge_notoll,sm_bridge_notoll,dum_bridge_notoll,rsr_bridge_notoll,
                                             carq_bridge_notoll,bm_bridge_notoll,ant_bridge_notoll), ~ .x==1),1,0),
          bata_bridges=if_else(if_any(c(bay_bridge,sm_bridge,dum_bridge,rsr_bridge,
                                        carq_bridge,bm_bridge,ant_bridge), ~ .x==1),1,0)) %>% 
-  select(trip_id,
-         bay_bridge_toll,
-         bay_bridge_notoll,
-         bay_bridge,
-         sm_bridge_toll,
-         sm_bridge_notoll,
-         sm_bridge,
-         dum_bridge_toll,
-         dum_bridge_notoll,
-         dum_bridge,
-         rsr_bridge_toll,
-         rsr_bridge_notoll,
-         rsr_bridge,
-         carq_bridge_toll,
-         carq_bridge_notoll,
-         carq_bridge,
-         bm_bridge_toll,
-         bm_bridge_notoll,
-         bm_bridge,
-         ant_bridge_toll,
-         ant_bridge_notoll,
-         ant_bridge,
-         gg_bridge_toll,
-         gg_bridge_notoll,
-         gg_bridge,
-         bata_bridges_toll,
-         bata_bridges_notoll,
-         bata_bridges,
-         sr37_80_to_mare,
-         sr37_mare_to_121,
-         sr37_121_to_101,
-         sr37_80_to_101,
-         i880_baybridge_to_237,
-         #i680_80_to_580_portion,      # Remove this as it's only a temporary variable
-         i680_80_to_580,
-         i680_580_to_101,
-         sr4_80_to_160,
-         i580_hayward_to_sanjoaquin,
-         i580_hayward_to_baybridge,
-         i80_13_to_580,
-         i80_580_to_Carquinez,
-         i80_680_to_12) 
+  left_join(.,trip %>% select(trip_id,express_lane_use),by="trip_id") %>% 
+  mutate(i880_baybridge_to_237_exp=if_else((i880_baybridge_to_237==1 & express_lane_use==1),1,0),
+         i680_bm_bridge_to_580_exp=if_else((i680_bm_bridge_to_580==1 & express_lane_use==1),1,0)) %>% 
+ select(trip_id,
+        bay_bridge_toll,
+        bay_bridge_notoll,
+        bay_bridge,
+        sm_bridge_toll,
+        sm_bridge_notoll,
+        sm_bridge,
+        dum_bridge_toll,
+        dum_bridge_notoll,
+        dum_bridge,
+        rsr_bridge_toll,
+        rsr_bridge_notoll,
+        rsr_bridge,
+        carq_bridge_toll,
+        carq_bridge_notoll,
+        carq_bridge,
+        bm_bridge_toll,
+        bm_bridge_notoll,
+        bm_bridge,
+        ant_bridge_toll,
+        ant_bridge_notoll,
+        ant_bridge,
+        gg_bridge_toll,
+        gg_bridge_notoll,
+        gg_bridge,
+        bata_bridges_toll,
+        bata_bridges_notoll,
+        bata_bridges,
+        sr37_80_to_mare,
+        sr37_mare_to_121,
+        sr37_121_to_101,
+        sr37_80_to_101,
+        i880_baybridge_to_237,
+        i880_baybridge_to_237_exp,
+        i680_80_to_580,
+        i680_bm_bridge_to_580,
+        i680_bm_bridge_to_580_exp,
+        i680_580_to_101,
+        sr4_80_to_160,
+        i580_hayward_to_sanjoaquin,
+        i580_hayward_to_baybridge,
+        i80_13_to_580,
+        i80_580_to_Carquinez,
+        i80_680_to_505) 
 
 # Export CSV of trips for joining with trip file
 
@@ -173,7 +176,7 @@ write.csv(final,file.path(BOX_dir,"Data","2023","Survey Conflation","BATS 2023 F
 # Summarize number of trips by facility, unweighted and weighted, for trips with >0 weight, output CSV
 
 trips_with_weights <- final %>%
-  left_join(.,trip,by="trip_id") %>% 
+  left_join(.,trip %>% select(trip_id,trip_weight_rmove_only),by="trip_id") %>% 
   filter(trip_weight_rmove_only>0) 
 
 trips_by_facility_sum <- trips_with_weights %>%  
