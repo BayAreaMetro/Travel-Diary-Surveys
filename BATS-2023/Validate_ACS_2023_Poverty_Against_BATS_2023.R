2904f777c84a8f3a7c4c431fa4c8a0c8b3df375d
Shimon Israel <sisrael@bayareametro.gov>
2025-01-02 16:33:33 -0800

    Brought in ACS 2023 data for comparison

--- BATS-2023/Validate_ACS_2023_Poverty_Against_BATS_2023.R
+++ BATS-2023/Validate_ACS_2023_Poverty_Against_BATS_2023.R
@@ -3,6 +3,8 @@
 
 # Bring in libraries
 suppressMessages(library(tidyverse))
+library(tidycensus)
+
 
 
 # Set file directories for input and output
@@ -40,13 +42,72 @@ combined <- left_join(derived,household_df,by="hh_id")
 
 person_combined <- left_join(person_df,combined,by="hh_id")
 
-final <- person_combined %>% 
+bats_cleaned <- person_combined %>% 
   group_by(home_county,poverty_status) %>% 
   summarize(Total=sum(person_weight)) %>% 
   pivot_wider(names_from=poverty_status,values_from=Total) %>% 
   select(County=home_county,under_2x_poverty,over_2x_poverty) %>% 
-  mutate(Total=under_2x_poverty+over_2x_poverty)
+  mutate(Total=under_2x_poverty+over_2x_poverty,Source="BATS") %>% 
+  ungroup()
 
+bay_bats <- bats_cleaned %>% 
+  summarize(County="Bay Area", under_2x_poverty=sum(under_2x_poverty),over_2x_poverty=sum(over_2x_poverty),
+            Total=sum(Total), Source="BATS")
+
+marin_napa_bats <- bats_cleaned %>%
+  filter(County %in% c("Marin","Napa")) %>% 
+  summarize(County="Marin_Napa", under_2x_poverty=sum(under_2x_poverty),over_2x_poverty=sum(over_2x_poverty),
+            Total=sum(Total), Source="BATS")
+
+combined_bats <- bind_rows(bats_cleaned,marin_napa_bats,bay_bats)
+
+share_bats <- combined_bats %>% 
+  mutate(under_2x_share=under_2x_poverty/Total) %>% 
+  select(County,under_2x_share,Source)
+
+# Now download ACS 2023 poverty data
+
+bay_counties <- c("Alameda", "Contra Costa", "Marin", "Napa", "San Francisco", "San Mateo",
+                  "Santa Clara", "Solano","Sonoma")
+
+acs_poverty_2023 <- get_acs(
+  geography = "county",
+  table = "C17002",
+  year = 2023,  
+  state = "CA",  
+  county = bay_counties,
+  survey = "acs1",
+  output = "wide"
+)
+
+# Remove MOE values
+
+acs_cleaned <- acs_poverty_2023 %>% 
+  mutate(NAME = str_remove(NAME, " County, California$"),
+         under_2x_poverty=C17002_001E-C17002_008E,
+         over_2x_poverty=C17002_008E,
+         Source="ACS") %>% 
+  rename(County=NAME, Total=C17002_001E) %>% 
+  select(-matches("_00[1-8]"),-GEOID) %>% 
+  relocate(Total,.after = over_2x_poverty)
+
+bay_acs <- acs_cleaned %>% 
+  summarize(County="Bay Area", under_2x_poverty=sum(under_2x_poverty),over_2x_poverty=sum(over_2x_poverty),
+            Total=sum(Total), Source="ACS")
+
+marin_napa_acs <- acs_cleaned %>%
+  filter(County %in% c("Marin","Napa")) %>% 
+  summarize(County="Marin_Napa", under_2x_poverty=sum(under_2x_poverty),over_2x_poverty=sum(over_2x_poverty),
+            Total=sum(Total), Source="ACS")
+
+combined_acs <- bind_rows(acs_cleaned,marin_napa_acs,bay_acs)
+
+share_acs <- combined_acs %>% 
+  mutate(under_2x_share=under_2x_poverty/Total) %>% 
+  select(County,under_2x_share,Source)
+
+combined_data <- bind_rows(share_bats,share_acs)
+  
 # Output file
 
-write.csv(final,file=file.path(output_dir,"BAT2023_SepDeliverable_Review","BATS_2023_Poverty_Summary.csv"),row.names=F)
+write.csv(combined_data,file=file.path(output_dir,"BAT2023_SepDeliverable_Review","BATS_2023_Poverty_Summary.csv"),row.names=F)
